7f68905649b8951d648167a275cda060bae15b93
adamkarmander <71024067+adamkarmander@users.noreply.github.com>
2022-01-22 13:26:27 +0100

    Insert input field values in table

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,14 @@
 export default function Form() {
     function addToTable() {
+        let firstName = document.getElementById('firstName').value;
+        let lastName = document.getElementById('lastName').value;
+        let phoneNumber = document.getElementById('phoneNumber').value;
+
         let newRow = document.createElement('tr');
         newRow.innerHTML = ' \
-        <td class="text-white border border-gray-400 pl-1">Adam</td> \
-        <td class="text-white border border-gray-400 pl-1">Kärmander</td> \
-        <td class="text-white border border-gray-400 pl-1">0738433504</td> \
+        <td class="text-white border border-gray-400 pl-1">' + firstName + '</td> \
+        <td class="text-white border border-gray-400 pl-1">' + lastName + '</td> \
+        <td class="text-white border border-gray-400 pl-1">' + phoneNumber + '</td> \
         ';
 
         document.getElementById('phoneBookTable').appendChild(newRow);
@@ -15,9 +19,9 @@ export default function Form() {
             <p className="text-3xl">Add a new contact:</p>
             <div className="w-full flex justify-center items-center">
             <div className="mr-5 flex-col flex justify-start items-start">
-            <label className="m-1 pl-1 bg-cyan-400 shadow-xl rounded-lg">First name: <input type="text" className="pl-1 rounded-r-lg" defaultValue="Coder"></input></label>
-            <label className="m-1 pl-1 bg-cyan-400 shadow-xl rounded-lg">Last name: <input type="text" className="pl-1 rounded-r-lg" defaultValue="Byte"></input></label>
-            <label className="m-1 pl-1 bg-cyan-400 shadow-xl rounded-lg">Phone number: <input type="text" className="pl-1 rounded-r-lg" defaultValue="8885559999"></input></label>
+            <label className="m-1 pl-1 bg-cyan-400 shadow-xl rounded-lg">First name: <input type="text" className="pl-1 rounded-r-lg" id="firstName" defaultValue="Coder"></input></label>
+            <label className="m-1 pl-1 bg-cyan-400 shadow-xl rounded-lg">Last name: <input type="text" className="pl-1 rounded-r-lg" id="lastName" defaultValue="Byte"></input></label>
+            <label className="m-1 pl-1 bg-cyan-400 shadow-xl rounded-lg">Phone number: <input type="text" className="pl-1 rounded-r-lg" id="phoneNumber" defaultValue="8885559999"></input></label>
             </div>
             <button className="ml-5 p-1 bg-green-400 hover:bg-green-300 shadow-xl rounded-lg" onClick={addToTable}>Add new contact</button>
             </div>
